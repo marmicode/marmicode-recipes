46a9ca744952c9eea1ac2e98904083f7d8516e26
Younes Jaaidi <yjaaidi@gmail.com>
2020-10-08 12:49:04 +0200

    ✅ add <mc-cart-info>

--- src/app/cart-info.component.spec.ts
+++ src/app/cart-info.component.spec.ts
@@ -1,3 +1,4 @@
+import { Animal, Cart } from './cart.service';
 import { CartInfoComponent } from './cart-info.component';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
@@ -15,5 +16,16 @@ describe('CartInfoComponent', () => {
     component = fixture.componentInstance;
   });
 
-  xit(`🚧 should show cart's total price`, () => {});
+  it(`should show cart's total price`, () => {
+    const cart = TestBed.inject(Cart);
+
+    jest.spyOn(cart, 'getAnimals').mockReturnValue([
+      { name: 'Missy', price: 200 },
+      { name: 'Doline', price: 100 },
+    ] as Animal[]);
+
+    fixture.detectChanges();
+
+    expect(fixture.nativeElement.textContent).toEqual('€300.00');
+  });
 });
--- src/app/cart-info.component.ts
+++ src/app/cart-info.component.ts
@@ -1,11 +1,26 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, NgModule } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  NgModule,
+  OnInit,
+} from '@angular/core';
+import { Animal, Cart } from './cart.service';
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'mc-cart-info',
-  template: `🚧 &lt;mc-cart-info&gt;`,
+  template: `{{ totalPrice | currency: 'EUR' }}`,
 })
-export class CartInfoComponent {}
+export class CartInfoComponent implements OnInit {
+  totalPrice: number;
+  constructor(private _cart: Cart) {}
+
+  ngOnInit(): void {
+    this.totalPrice = this._cart
+      .getAnimals()
+      .reduce((total, animal) => total + animal.price, 0);
+  }
+}
 
 @NgModule({
   declarations: [CartInfoComponent],
